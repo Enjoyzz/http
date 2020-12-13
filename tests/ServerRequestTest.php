9e37f563a2e567206a098e8496333b69ead3401f
Enjoys <deadly.info@gmail.com>
2020-12-13 23:11:24 +0300

    add tests

--- tests/ServerRequestTest.php
+++ tests/ServerRequestTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Enjoys\Http;
 
 use Enjoys\Http\ServerRequest;
+use HttpSoft\ServerRequest\ServerRequestCreator;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -20,25 +21,91 @@ class ServerRequestTest extends TestCase
         $request = new ServerRequest();
         $this->assertSame($_SERVER, $request->server());
         $this->assertSame($_SERVER['PHP_SELF'], $request->server('PHP_SELF'));
+        $this->assertSame(null, $request->server('NOT_ISSET'));
     }
 
     public function testPost()
     {
+        $request = new ServerRequest(
+            new \HttpSoft\Message\ServerRequest(
+                [], [], [], [], [
+                      'test' => 'value'
+                  ]
+            )
+        );
+        $this->assertSame('value', $request->post('test'));
+        $this->assertSame(['test' => 'value'], $request->post());
+        $this->assertSame('default_value', $request->post('not_isset', 'default_value'));
     }
 
-    public function testFiles()
+
+    public function methods()
     {
+        return [
+            ['POST'],
+            ['GET'],
+            ['PUT'],
+            ['DELETE'],
+        ];
     }
 
-    public function testGetMethod()
+    /**
+     * @dataProvider methods
+     */
+    public function testGetMethod($method)
     {
+        $request = new ServerRequest(
+            ServerRequestCreator::createFromGlobals(
+                [
+                    'REQUEST_METHOD' => $method
+                ]
+            )
+        );
+        $this->assertSame($method, $request->getMethod());
     }
 
     public function testAddQuery()
     {
+        $request = new ServerRequest(
+            new \HttpSoft\Message\ServerRequest(
+                [], [], [], [
+                      'test' => 'value'
+                  ]
+            )
+        );
+        $request->addQuery(
+            [
+                'test2' => 'value2'
+            ]
+        );
+
+        $this->assertSame(['test' => 'value', 'test2' => 'value2'], $request->get());
+
+        $request->addQuery(
+            [
+                'test' => 'value3'
+            ]
+        );
+
+        $this->assertSame(['test' => 'value3', 'test2' => 'value2'], $request->get());
     }
 
     public function testGet()
     {
+        $request = new ServerRequest(
+            new \HttpSoft\Message\ServerRequest(
+                [], [], [], [
+                      'test' => 'value'
+                  ]
+            )
+        );
+        $this->assertSame('value', $request->get('test'));
+        $this->assertSame(['test' => 'value'], $request->get());
+        $this->assertSame('default_value', $request->get('not_isset', 'default_value'));
     }
+
+
+//    public function testFiles()
+//    {
+//    }
 }
